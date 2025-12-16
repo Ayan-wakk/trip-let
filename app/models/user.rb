05889f9f313e13f1ce6f2c8935e016584c53b215
Ayan Wakk <ayami.k1990.0904@gmail.com>
2025-12-17 00:25:33 +0900

    新規登録画面・ログイン画面の実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,10 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable,
-         :registerable,
-         :recoverable,
-         :rememberable,
-         :validatable,
-         :omniauthable, omniauth_providers: %i[google_oauth2 twitter2]
+  devise :database_authenticatable, # データーベースでのパスワード認証
+         :registerable, #ユーザー登録
+         :recoverable, # パスワードリセット
+         :rememberable, # ログイン情報の記憶
+         :validatable, # メール・パスワードのバリデーション
+         :omniauthable, omniauth_providers: %i[google_oauth2 twitter2] # Google, Twitter での OAuth 認証
 end

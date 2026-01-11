180892afaaa79ee53fa579a556c3f9e4d8bc8527
Ayan Wakk <ayami.k1990.0904@gmail.com>
2026-01-11 19:17:00 +0900

    規約・ポリシー・お問い合わせの実装

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,10 @@ Rails.application.routes.draw do
     omniauth_callbacks: "users/omniauth_callbacks"
   }
 
-  root "pages#home"
+  root "pages#top"
+  get "/terms", to: "pages#terms"
+  get "/privacy", to: "pages#privacy"
+  get "/operator", to: "pages#operator"
   resources :posts, only: %i[index new create show edit destroy update]
   get "up" => "rails/health#show", as: :rails_health_check
 end

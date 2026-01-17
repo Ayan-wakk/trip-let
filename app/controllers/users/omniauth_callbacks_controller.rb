c9a82f3d016befeaa3350f022909c4862820a0d1
Ayan Wakk <ayami.k1990.0904@gmail.com>
2026-01-17 13:28:44 +0900

    Debug: Googleログインのバリデーションエラーを可視化

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -9,6 +9,20 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     end
   end
 
+  def google_oauth2
+    @user = User.from_omniauth(request.env["omniauth.auth"])
+
+    if @user.persisted?
+      sign_in_and_redirect @user, event: :authentication
+      set_flash_message(:notice, :success, kind: "Google") if is_navigational_format?
+    else
+      # ⬇️ これを一時的に追加してログでエラーを確認
+      Rails.logger.error(@user.errors.full_messages) 
+      session["devise.google_data"] = request.env["omniauth.auth"].except(:extra)
+      redirect_to new_user_registration_url, alert: @user.errors.full_messages.join("\n")
+    end
+  end
+
   def twitter2
     handle_auth "Twitter"
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -11,12 +11,24 @@ class User < ApplicationRecord
 
   has_many :posts, dependent: :destroy
 
-  def self.from_omniauth(auth)
-    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
-      user.email = auth.info.email
-      user.name  = auth.info.name
-      user.password = Devise.friendly_token[0, 20]
-      user.skip_confirmation!
+def self.from_omniauth(auth)
+    # ユーザーを探すか、新しく作る準備をする
+    user = where(provider: auth.provider, uid: auth.uid).first_or_initialize do |u|
+      u.email = auth.info.email
+      u.name  = auth.info.name
+      u.password = Devise.friendly_token[0, 20]
     end
+
+    # 新規登録ならメール確認をスキップ
+    user.skip_confirmation! if user.new_record?
+
+    # 保存を試み、失敗したらログに出す
+    unless user.save
+      Rails.logger.error "--------------------------------------------------"
+      Rails.logger.error "Googleログイン保存失敗: #{user.errors.full_messages.join(', ')}"
+      Rails.logger.error "--------------------------------------------------"
+    end
+
+    user # 最後にuserを返してあげる必要があります
   end
 end

0d934c59f51581edf342e7472225f20ccbc15b0a
Ayan Wakk <ayami.k1990.0904@gmail.com>
2026-02-02 22:03:38 +0900

    編集の権限追加

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,8 @@
 class PostsController < ApplicationController
   before_action :authenticate_user!, only: [:show, :new, :edit, :update, :destroy]
   before_action :authenticate_user!, only: :index, if: -> { params[:scope] == "my" }
-  before_action :set_post, only: [ :show, :edit, :update, :destroy ]
+  before_action :set_post, only: [ :show ]
+  before_action :set_my_post, only: [ :edit, :update, :destroy ]
 
   def index
     @is_my_page = params[:scope] == "my"
@@ -23,8 +24,6 @@ class PostsController < ApplicationController
   end
 
   def show
-    @post = Post.find(params[:id])
-
     unless @post.is_public? || @post.user == current_user # 投稿が公開されているまたは投稿者本人であるこのどちらでもない場合
       redirect_to posts_path, alert: "この投稿は非公開です"
     end
@@ -73,6 +72,10 @@ class PostsController < ApplicationController
 
   def set_post
     @post = Post.find(params[:id])
+  end
+
+  def set_my_post
+    @post = current_user.posts.find(params[:id])
   end
 
   def post_params
